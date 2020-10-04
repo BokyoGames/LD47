521e18b6caa466377e03d54b8fea58e6222058c2
Danger <danger.recheng@hotmail.it>
2020-10-04 19:01:08 +0200

    feat(star): fix star collector

--- Assets/Scripts/StarCollector.cs
+++ Assets/Scripts/StarCollector.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class StarCollector : AbstractEmitter
 {
     public float collector_force = 1;
+    private PlayerInfo player_info;
     // Start is called before the first frame update
     void Start()
     {
+        player_info = GameObject.Find("PlayerInfo").GetComponent<PlayerInfo>();
         tower_beat = GetComponent<AudioSource>();
         tower_cannon = transform.Find("Turret_Cannon").gameObject;
         period = 4f/spawn_track_vector.Length;
@@ -50,6 +52,16 @@ public class StarCollector : AbstractEmitter
         {
             Vector3 force_forward = this.gameObject.transform.TransformDirection(this.gameObject.transform.position);
             drop.GetComponent<Rigidbody2D>().AddForce(force_forward * collector_force);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if(collision.gameObject.CompareTag("Drop"))
+        {
+            CollectOnClick drop = collision.gameObject.GetComponent<CollectOnClick>();
+            player_info.music_energy += drop.loot_value;
+            Destroy(collision.gameObject);
         }
     }
 }
